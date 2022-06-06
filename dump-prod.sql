9962a93fd56cb6aac658121cf888e8432d1ea094
PaoloMessina <phemt.latd@gmail.com>
2022-06-06 17:01:54 +0200

    Update dump-prod.sql

--- dump-prod.sql
+++ dump-prod.sql
@@ -962,6 +962,18 @@ ALTER TABLE public.d_prosa_sanzioni ALTER COLUMN importo_pagamento_misura_ridott
 
 /* END FIX ALTER TABLE FOR PMR import with comma */
 
+/* START FIX ALTER TABLE FOR Marche Modelli */
+
+ALTER TABLE public.d_adi_veicoli ADD desc_modello varchar(200) NOT null default '';
+
+ALTER TABLE public.d_adi_veicoli ADD desc_marca varchar(200) NOT NULL default '';
+
+ALTER TABLE public.d_adi_veicoli DROP CONSTRAINT fk_seq_id_modelli_veicoli;
+
+ALTER TABLE public.d_adi_veicoli DROP COLUMN fk_seq_id_modelli_veicoli;
+
+/* END FIX ALTER TABLE FOR Marche Modelli */
+
 --
 -- TOC entry 3681 (class 0 OID 50251)
 -- Dependencies: 201
